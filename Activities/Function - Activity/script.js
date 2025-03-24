18f084290d32c64d0dc806968d2a00219c05322a
koykoy12345 <tosposoc@gmail.com>
2025-03-24 15:34:43 +0800

    Function Activity 1

---
+++ Activities/Function - Activity/script.js
@@ -0,0 +1,14 @@
+ function whatShallIwear (temp){
+ if (tem< 60) {
+    console.log("wear a jacket");
+ }
+ else if (temp < 70) {
+    console.log("wear a sweater");
+ } else {
+   console.log("wear t-shirt")
+ }
+ }
+ 
+ whatShallIwear(50);
+ whatShallIwear(60);
+ whatShallIwear(80);
